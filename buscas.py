d484a23bfd17ff5d043d99c6a27c3ced7b4cd1d5
ninivehelen <ninivehelen@gmail.com>
2024-03-20 09:20:49 -0300

    ajuste das cores dos gŕaficos, ajuste para variavel q assumir valores entre 10^2 10^3 10^4 10^5 e ajuste da funçã gráfico eixo X para q e n e para eixo Y tempo

--- buscas.py
+++ buscas.py
@@ -5,26 +5,31 @@ import seaborn as sns
 import matplotlib.pyplot as plt 
 
 #Função para gerar os valores aletorios de n 
-def criar_lista_n(valor):
-    valor_tam = list(str(valor))
-    tam = len(valor_tam)
-    valor_tam = tam - 1
-    tam = 10**valor_tam
-    n = np.random.randint(0,tam,tam)
+def criar_lista_n(valor_n):
+    #capturando o valor e trasformando em lista
+    valor_tam_n = list(str(valor_n))
+    #Quantidade do tamanho de valor
+    tam_n = len(valor_tam_n)
+    #diminuido um valor para ficar a quantidade correta, pois o numero 1 não entra na contagem do valor 1000, 10000 etc..
+    valor_tam_n = tam_n - 1
+    tam_n = 10**valor_tam_n
+    #gerando o vetor de numeros aleatorios
+    n = np.random.randint(0,tam_n,tam_n)
     return n
 #Função para gerar os valores aletorios de q
-def criar_lista_q(valor):
-    q = np.random.randint(1, 100000, 100000)
-    valor_tam = list(str(valor))
-    tam = len(valor_tam)
-    valor_tam = tam - 1
-    tam = 10**valor_tam
-    q = np.random.randint(0,tam,tam)
+def criar_lista_q():
+    #possiveis quantidades que q pode ter de 10^2 até 10^5 de forma aleatoria
+    possiveis_quantidades = [10**2, 10**3, 10**4, 10**5]
+    #para escolher a quantidade
+    quantidade = np.random.choice(possiveis_quantidades)
+    #Gerando os valores aleatorios
+    q = np.random.randint(0, quantidade, quantidade)
     return q
 
 #Função busca sequencial 
-def busca_sequencial_linear(n,q,valor, nome):
-     print(f'Processando a função busca sequencial. Quantidade de números buscados: {valor}')
+def busca_sequencial_linear(n,q,valor_n,tam_q, nome):
+    #está função pecorre  n e verificando se o q estã contido em n
+     print(f'Processando a função busca sequencial. Quantidade de números em n: {valor_n}')
      ocorrencia_na_lista = []
      inicio = time.time()
      for i in q:
@@ -35,12 +40,14 @@ def busca_sequencial_linear(n,q,valor, nome):
      fim = time.time()
      tempo_total = (round(float(fim - inicio), 3))
      print("Função busca sequencial finalizada")
-     salvar_dados(valor,tempo_total, nome)
+     salvar_dados(valor_n,tam_q,tempo_total,nome)
      return ocorrencia_na_lista
 
 #Função busca sequencia otimizada  
-def busca_sequencial_otimizada(n,q,valor, nome):
-    print(f'Processando a função busca sequencial otimizada. Quantidade de números buscados: {valor}')
+def busca_sequencial_otimizada(n,q,valor_n,tam_q, nome):
+    #está função ordena n de forma crescente e veririca se n for maior que q para volta pro inicio da lista e verifica o proximo q
+    #está função pecorre  n e verificando se o q estã contido em n
+    print(f'Processando a função busca sequencial otimizada. Quantidade de números em n: {valor_n}')
     ocorrencia_na_lista_automatizada = []
     inicio = time.time()
     n.sort()
@@ -55,12 +62,14 @@ def busca_sequencial_otimizada(n,q,valor, nome):
     tempo_total = fim - inicio
     tempo_total = (round(float(fim - inicio), 3))
     print("Função busca sequencial otimizada finalizada")
-    salvar_dados(valor,tempo_total,nome)
+    salvar_dados(valor_n,tam_q,tempo_total,nome)
     return  ocorrencia_na_lista_automatizada
 
 #Função busca binária
-def busca_binaria(n,q,valor, nome, inicio_lista=0, fim_lista=None):
-    print(f'Processando a função busca binária. Quantidade de números buscados: {valor}')
+def busca_binaria(n,q,valor_n, tam_q, nome, inicio_lista=0, fim_lista=None):
+     #essa  comparar q o primeiro elemento o elemento buscado com o n central
+     #forma que uma das metades possa ser desprezada.
+    print(f'Processando a função busca binária. Quantidade de números em n: {valor_n}')
     encontrados = []
     if fim_lista is None:
        fim_lista = len(n) - 1
@@ -82,49 +91,48 @@ def busca_binaria(n,q,valor, nome, inicio_lista=0, fim_lista=None):
     tempo_total = fim - inicio
     tempo_total = (round(float(fim - inicio), 3))
     print("Função busca binária finalizada")
-    salvar_dados(valor,tempo_total,nome)
+    salvar_dados(valor_n,tam_q,tempo_total,nome)
     return encontrados
     
-def salvar_dados(valor,tempo,nome):
+def salvar_dados(valor_n,tam_q, tempo,nome):
+  #verificando se o arquivo já existe e se não criando o data frame para salvar os dados de valor n e tempo da função
   try:
     df = pd.read_csv(f'{nome}.csv')
   except FileNotFoundError:
-    df = pd.DataFrame(columns=['Valor', 'Tempo'])
-  
-  df_novo = pd.DataFrame({'Valor': [valor], 'Tempo': [tempo], 'Função': [nome]})
+    df = pd.DataFrame(columns=['Valor_n','Valor_q','Tempo'])
+
+  #salvando os arquivos de cada valor e tempo de função processada
+  df_novo = pd.DataFrame({'Valor_n': [valor_n], 'Valor_q':[tam_q],'Tempo': [tempo], 'Função': [nome]})
   df = pd.concat([df, df_novo], ignore_index=True)
   df.to_csv(f'{nome}.csv', index=False)
   print(f'arquivo salvo {nome}')
 
 #Função de criar o gráfico
-def criar_grafico(nome,core):
+def criar_grafico(nome, cor):
     df = pd.read_csv(f'{nome}.csv')
-    troca_valores = {10000: '10^4', 100000: '10^5', 1000000: '10^6', 10000000: '10^7'}
-    df['Valor'] = df['Valor'].map(troca_valores)
-    
+    #convertendo os valores 100000 etc para 10^ para ajustar no gráfico
+    troca_valores_n = {10000: '10^4', 100000: '10^5', 1000000: '10^6', 10000000: '10^7'}
+    troca_valores_q = {100: '10^2', 1000: '10^3', 10000: '10^4', 100000: '10^5'}
+    df['Valor_n'] = df['Valor_n'].map(troca_valores_n)
+    df['Valor_q'] = df['Valor_q'].map(troca_valores_q)
+
+    #criando o gráfico de linha
+    sns.set()
     plt.figure()
-    sns.set_style("whitegrid")
-    sns.lineplot(data=df, x='Valor', y='Tempo', hue='Função',palette=[core])
+    df = df.sort_values(by='Valor_n')
+    df = df.rename(columns={'Valor_q': 'Quantidade de Números q'})
+    sns.lineplot(data=df, x='Valor_n', y='Tempo', hue='Quantidade de Números q',palette=[cor], style='Quantidade de Números q',markers=True,lw=1,markeredgewidth=1.25,markeredgecolor='black',markersize=7,errorbar=None,markerfacecolor='white', dashes=False)
+    plt.plot(df['Valor_n'], df['Tempo'], marker='', color=cor, linewidth=2, linestyle='-')
 
-    plt.xlabel('Quantidade de números buscado',fontsize=10, fontweight='bold')
-    plt.ylabel('Tempo em segundos',fontsize=10, fontweight='bold')
-
-    x = df['Valor']
-    y = df['Tempo']
+    # Ajustando a legenda
+    plt.legend(title='Quantidade de Números q', loc='best')
     
-    plt.yticks(y)
-    plt.scatter(x, y, marker="*", color='red')
-    
-
-    plt.xlabel('Quantidade de Números Buscado')
+    #colocando titulo e x e y
+    plt.xlabel('Quantidade de Números n')
     plt.ylabel('Tempo em Segundos')
-
-    num_ticks = 13 
-    y_min = df['Tempo'].min()
-    y_max = df['Tempo'].max()
-    y_ticks = np.linspace(y_min, y_max, num_ticks)
-
-    plt.yticks(y_ticks)
+    #titulo do gráfico
+    plt.title(f'Gráfico de Linha de Tempo para {nome}')
+    #Salvndo
     plt.savefig(f'{nome}.png', format='png')
     plt.close() 
     print("gráfico criado")
--- main.py
+++ main.py
@@ -7,12 +7,12 @@ nome_busca_binaria = "Busca_Binária"
 
 #cores de cada gráfico
 #Valores que n e q podem assumir
-valores = [10000,100000]
+valores = [10000,100000,1000000,10000000]
 
 #cores dos gráficos
-cor_sequencial = "#0d633d"
-cor_sequencial_otimizada = "#0ead66"
-cor_busca_binaria = "#15ed8c"
+cor_sequencial = "#044026"
+cor_sequencial_otimizada = "#14c478"
+cor_busca_binaria = "#4af7ac"
 
 #Para escolher qual função quer rodar para facilitar.
 busca_sequencial_linear_compila = True
@@ -21,30 +21,32 @@ busca_binaria_compila = True
 
 #Função principal
 def main():
-     for valor in valores:
+     for valor_n in valores:
        #passando os valores para gerar os numeros aletorios
-        n = criar_lista_n(valor)
-        q = criar_lista_q(valor)
+        n = criar_lista_n(valor_n)
+        q = criar_lista_q()
         n = n.tolist()
         q = q.tolist()
-        print(f'Vetor aleatório n gerado para números: {valor}')
-        print(f'Vetor aleatório q gerado para números: {valor}')
+        tam_q = len(q)
+        print(f'Vetor aleatório n gerado para: {valor_n}')
+        print(f'Vetor aleatório q gerado para: {tam_q}')
         #Chamando as funções que estão com True para compilar
         if busca_sequencial_linear_compila == True:
-             print(len(busca_sequencial_linear(n,q,valor,nome_sequencial)))
+             print(len(busca_sequencial_linear(n,q,valor_n,tam_q,nome_sequencial)))
              criar_grafico(nome_sequencial,cor_sequencial)
-
+         
         if busca_sequencial_linear_otimizada_compila == True:
-             print(len(busca_sequencial_otimizada(n,q,valor,nome_sequencial_otimizada)))
+             print(len(busca_sequencial_otimizada(n,q,valor_n, tam_q,nome_sequencial_otimizada)))
              criar_grafico(nome_sequencial_otimizada,cor_sequencial_otimizada)
 
         if busca_binaria_compila == True:
-             print(len(busca_binaria(n,q,valor,nome_busca_binaria)))
+             print(len(busca_binaria(n,q,valor_n,tam_q,nome_busca_binaria)))
              criar_grafico(nome_busca_binaria ,cor_busca_binaria)
 
         elif busca_sequencial_linear_compila == False and busca_sequencial_linear_otimizada_compila == False and busca_sequencial_binaria_compila == False:
           print("Nenhuma função escolhida para ser processada")
-          
+
+#função principal que compila o código          
 if __name__ == "__main__":
     main()
     
